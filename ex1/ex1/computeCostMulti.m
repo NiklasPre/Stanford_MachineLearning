636af5c3b9bd9078fca2c5fba197fe2c4ef7ca38
Niklas Preschern <niklas.preschern@gmx.at>
2016-07-21 16:16:19 +0200

    Commit 2

--- ex1/ex1/computeCostMulti.m
+++ ex1/ex1/computeCostMulti.m
@@ -12,11 +12,9 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
-
+for i = 1:m
+    J = J + (1/(2*m)*(theta' * X(i,:)' - y(i)) ^ 2);
+end
 % =========================================================================
-
+display(J);
 end
--- ex1/ex1/gradientDescentMulti.m
+++ ex1/ex1/gradientDescentMulti.m
@@ -15,35 +15,21 @@ for iter = 1:num_iters
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
-<<<<<<< HEAD
     % 
 
     tmp1 = 0;
     tmp2 = 0;
+    tmp3 = 0;
     
     for i = 1:m
         tmp1 = tmp1 + (theta' * X(i,:)' - y(i));
         tmp2 = tmp2 + (theta' * X(i,:)' - y(i))*X(i,2);
+        tmp3 = tmp3 + (theta' * X(i,:)' - y(i))*X(i,3);
+
     end
-    
-    %theta(1,1) = tmp1;
-    %theta(2,1) = tmp2;
     theta(1) = theta(1) - tmp1*(alpha/m);
     theta(2) = theta(2) - tmp2*(alpha/m);
-=======
-    %
-
-
-
-
-
-
-
-
-
-
->>>>>>> fd692cee753aa970032a587eab74fdea5d3cc230
-
+    theta(3) = theta(3) - tmp3*(alpha/m);
     % ============================================================
 
     % Save the cost J in every iteration    
